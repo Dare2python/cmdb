47598d529553b94e7ec25a7b8f927f5388d30cfe
Dare2python <dare2python@gmail.com>
2018-07-18 18:40:35 +0300

    shell context processor

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 #! /usr/bin/python
 
 import os
-from app import create_app
+from app import create_app, db
+from app.models import User, Item
 
 if __name__ == '__main__' or __name__ == 'run':
     app = create_app(os.environ.get('FLASK_CONFIG', 'development'))
@@ -9,3 +10,7 @@ if __name__ == '__main__' or __name__ == 'run':
         pass
 
     app.run()
+
+    @app.shell_context_processor
+    def make_shell_context():
+        return {'db': db, 'User': User, 'Item': Item}
