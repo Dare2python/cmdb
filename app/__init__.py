d454f82f59233767eb072f5230b90a578e0088fd
Dare2python <dare2python@gmail.com>
2018-07-18 18:22:49 +0300

    DB and migration via Alembic

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(config_name):
@@ -16,9 +19,12 @@ def create_app(config_name):
 
     # initialize extensions
     db.init_app(app)
+    migrate.init_app(app, db)
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
 
     return app
+
+from app import models
--- config/development.py
+++ config/development.py
@@ -4,8 +4,13 @@ import os
 DEBUG = True
 # IGNORE_AUTH = True
 SECRET_KEY = 'the-secret!'
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(
-    os.path.dirname(__file__), '../data-dev.sqlite3?check_same_thread=False')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                          'sqlite:///' + \
+                          os.path.join(basedir,
+                                       '../data-dev.sqlite3')
+#?check_same_thread=False')
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_RECORD_QUERIES = True
