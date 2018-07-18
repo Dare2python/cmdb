ac009ec1117a3f00c057b9a5de11c8b738a51c41
Dare2python <dare2python@gmail.com>
2018-07-18 15:39:22 +0300

    Login form with Flash messages in base template

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, url_for, request, g, \
     jsonify, current_app
 from app import db
+from app.main.forms import LoginForm
 from app.main import bp
 
 @bp.route('/', methods=['GET', 'POST'])
@@ -8,3 +9,12 @@ from app.main import bp
 def index():
     user = {'username': 'Serge'}
     return render_template('index.html', title='Home', user=user)
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('main.index'))
+    return render_template('login.html', title='Sign In', form=form)
